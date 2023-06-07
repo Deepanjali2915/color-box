ba7016d3670a8345029fde5801b74fb124800afe
Deepanjali <deepanjali20@navgurukul.org>
2023-06-07 19:13:47 +0530

    color box in js

---
+++ box.js
@@ -0,0 +1,50 @@
+let Box=document.getElementById("Box");
+console.log(Box)
+    
+let c=document.getElementById("Green");   
+c.onclick=function(){
+     Box.style.backgroundColor = "green";
+};    
+
+console.log(c)  
+
+let d=document.getElementById("Blue");   
+d.onclick=function(){
+    Box.style.backgroundColor = "blue";
+};    
+    
+console.log(d)  
+
+let e=document.getElementById("Purple");   
+e.onclick=function(){
+    Box.style.backgroundColor = "purple";
+  };    
+    
+console.log(e)  
+
+let f=document.getElementById("Yellow");   
+f.onclick=function(){
+    Box.style.backgroundColor = "yellow";
+  };    
+    
+console.log(f)  
+
+
+let g=document.getElementById("Orange");   
+g.onclick=function(){
+    Box.style.backgroundColor = "orange";
+  };    
+    
+console.log(g)  
+
+
+let h=document.getElementById("Pink");   
+h.onclick=function(){
+    Box.style.backgroundColor = "pink";
+  };    
+    
+console.log(h)  
+
+
+
+
